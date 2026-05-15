c7babbae4253e484003079ec20725beead913c8d
Link4Electronics <link4electronics@gmail.com>
2026-05-15 02:44:28 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,11 +7,10 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    kmines         \
-    kvantum        \
-    lxqt-qtplugin  \
-    pipewire-audio \
-    pipewire-jack  \
+    kmines        \
+    kvantum       \
+    libpipewire   \
+    lxqt-qtplugin \
     qt6ct
 
 echo "Installing debloated packages..."

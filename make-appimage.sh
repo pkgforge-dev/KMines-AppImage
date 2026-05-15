bb46d1f9296cc24fd627c1c191f73ecef88c8b0d
Link4Electronics <link4electronics@gmail.com>
2026-05-15 01:21:49 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,13 +8,13 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/
+export ICON=/usr/share/icons/hicolor/128x128/apps/kmines.png
 export DESKTOP=/usr/share/applications/org.kde.kmines.desktop
 export DEPLOY_QT=1
 export QT_DIR=qt6
 
 # Deploy dependencies
-quick-sharun /usr/bin/kmines
+quick-sharun /usr/bin/kmines /usr/share/kmines
 
 # Additional changes can be done in between here
 
